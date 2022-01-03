6f21589e7fbaf602b01e6a2986a08da3899e29e4
Admirl44 <36560341+admiral44@users.noreply.github.com>
2022-01-03 19:23:08 +0530

    master

--- Project-1/insert/form.php
+++ Project-1/insert/form.php
@@ -26,13 +26,30 @@
 
     <div class="container">
         <form>
-            <div class="">
+            <div class="row">
 
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Name</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" name="p_naem" placeholder="Enter Name">                    
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Number</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" name="p_no" placeholder="Enter Number">
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="p_email" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                </div>
+
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>

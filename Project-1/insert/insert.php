c527028090c4e9d1f47adfc9ef8ce463feaa543d
Admirl44 <36560341+admiral44@users.noreply.github.com>
2022-01-03 20:32:28 +0530

    master

--- Project-1/insert/insert.php
+++ Project-1/insert/insert.php
@@ -2,18 +2,28 @@
 
 include("../conn/conn.php");
 
-$p_name = $_REQUEST["p_name"];
-$p_no = $_REQUEST["p_no"];
-$p_email = $_REQUEST["p_email"];
 
-$sql = "INSERT INTO `Personal_info`(`p_name`, `p_no`, `p_email`) VALUES ('$p_name','$p_no','$p_email')";
+if(!isset($_POST["submit"])) {
 
-if ($conn->query($sql) === TRUE) {
-  echo "<br>New record created successfully";
-  header("Location: ../index.php");
+  if(($_REQUEST["p_name"] != "" ) && ($_REQUEST["p_no"] != "" ) && ($_REQUEST["p_email"] != "" )) {
+
+    $p_name = $_REQUEST["p_name"];
+    $p_no = $_REQUEST["p_no"];
+    $p_email = $_REQUEST["p_email"];
+
+    $sql = "INSERT INTO `Personal_info`(`p_name`, `p_no`, `p_email`) VALUES ('$p_name','$p_no','$p_email')";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "<br>New record created successfully";
+      header("Location: ../index.php");
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+  }else {
+    echo "<br>Please enter all information.<br>";
+  }
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "<br>Please click on submit.<br>";
 }
 
 $conn->close();
-?>
